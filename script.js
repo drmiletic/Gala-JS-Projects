64a1398bb363ce1246f4b752b724f9b2b881842a
Gala Miletic <dragica.miletic@gmail.com>
2019-03-16 22:18:38 +0100

    Adding JS step1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //Javascript
+
+function inputValue() {
+	var insertValue = event.target.value;
+		if (insertValue == [0-9]) {
+			document.input.onfocus.innerHTML = insertValue;
+		} else {
+			document.querySelector("#op").innerHTML = insertValue;
+			}
+}
+
+function calculate() {
+	var numberOne = Number(document.querySelector("#valueOne").value);
+	var numberTwo = Number(document.querySelector("#valueTwo").value);
+	var calcOp = document.querySelector("#op").innerHTML;
+	var result = eval(numberOne + calcOp + numberTwo);
+
+	// result output
+	document.querySelector("#result").innerHTML = result; 
+}
