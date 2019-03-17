d5c09ee9470ec9c468226fa5c0e3b4d440ef2317
Gala Miletic <dragica.miletic@gmail.com>
2019-03-17 09:40:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 //Javascript
 
 function inputValue() {
-	var insertValue = event.target.value;
-		if (insertValue == [0-9]) {
-			document.input.onfocus.innerHTML = insertValue;
-		} else {
-			document.querySelector("#op").innerHTML = insertValue;
-			}
+	var insertValue = event.target.innerHTML;
+		/* if (insertValue === [0-9]) {
+			document.input.onfocus.innerHTML += insertValue;
+		} else { */
+		document.querySelector("#op").innerHTML = insertValue;
+		//}
 }
 
 function calculate() {
 	var numberOne = Number(document.querySelector("#valueOne").value);
 	var numberTwo = Number(document.querySelector("#valueTwo").value);
-	var calcOp = document.querySelector("#op").innerHTML;
+	var calcOp = document.querySelector("#op").value;
+	//Replace eval with another solution
 	var result = eval(numberOne + calcOp + numberTwo);
 
 	// result output
 	document.querySelector("#result").innerHTML = result; 
+}
+
+function clearInput() {
+	//To clear all input fields - IDEA: something.innerHTML = ''
 }
